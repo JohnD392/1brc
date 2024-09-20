762663e13391d6c0f54a50720b6a667215947d50
John Dunn <johnd392@yahoo.com>
2024-09-19 21:09:57 -0700

    mmap and hashmap to another file

---
+++ hashmap.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"hash/fnv"
+)
+
+// KeyValue represents a key-value pair in the hashmap
+type KeyValue struct {
+	Key   []byte
+	Value *TempData
+}
+
+// HashMap represents our hashmap structure
+type HashMap struct {
+	buckets    []*KeyValue
+	size       int
+	bucketSize int
+}
+
+// NewHashMap creates a new HashMap with the given bucket size
+func NewHashMap(bucketSize int) *HashMap {
+	return &HashMap{
+		buckets:    make([]*KeyValue, bucketSize),
+		size:       0,
+		bucketSize: bucketSize,
+	}
+}
+
+// hash generates a hash for the given key
+func (hm *HashMap) hash(key []byte) int {
+	h := fnv.New32a()
+	h.Write(key)
+	return int(h.Sum32()) % hm.bucketSize
+}
+
+// Set adds a key-value pair to the hashmap
+func (hm *HashMap) Set(key []byte, value *TempData) {
+	index := hm.hash(key)
+	if hm.buckets[index] == nil {
+		hm.buckets[index] = &KeyValue{Key: key, Value: value}
+		hm.size++
+	} else if bytes.Equal(hm.buckets[index].Key, key) {
+		hm.buckets[index].Value = value
+	} else {
+		// Handle collision: find the next empty slot
+		for i := (index + 1) % hm.bucketSize; i != index; i = (i + 1) % hm.bucketSize {
+			if hm.buckets[i] == nil {
+				hm.buckets[i] = &KeyValue{Key: key, Value: value}
+				hm.size++
+				return
+			} else if bytes.Equal(hm.buckets[i].Key, key) {
+				hm.buckets[i].Value = value
+				return
+			}
+		}
+		// If we get here, the hashmap is full
+		fmt.Println("HashMap is full, cannot add more elements")
+	}
+}
+
+// Get retrieves the value associated with the given key
+func (hm *HashMap) Get(key []byte) (*TempData, bool) {
+	index := hm.hash(key)
+	if hm.buckets[index] != nil && bytes.Equal(hm.buckets[index].Key, key) {
+		return hm.buckets[index].Value, true
+	}
+	// Handle collision: search for the key
+	for i := (index + 1) % hm.bucketSize; i != index; i = (i + 1) % hm.bucketSize {
+		if hm.buckets[i] == nil {
+			break
+		}
+		if bytes.Equal(hm.buckets[i].Key, key) {
+			return hm.buckets[i].Value, true
+		}
+	}
+	return nil, false
+}
+
+// Delete removes a key-value pair from the hashmap
+func (hm *HashMap) Delete(key []byte) bool {
+	index := hm.hash(key)
+	if hm.buckets[index] != nil && bytes.Equal(hm.buckets[index].Key, key) {
+		hm.buckets[index] = nil
+		hm.size--
+		return true
+	}
+	// Handle collision: search for the key
+	for i := (index + 1) % hm.bucketSize; i != index; i = (i + 1) % hm.bucketSize {
+		if hm.buckets[i] == nil {
+			break
+		}
+		if bytes.Equal(hm.buckets[i].Key, key) {
+			hm.buckets[i] = nil
+			hm.size--
+			return true
+		}
+	}
+	return false
+}
+
+// Size returns the number of elements in the hashmap
+func (hm *HashMap) Size() int {
+	return hm.size
+}
+
--- main.go
+++ main.go
@@ -1,118 +1,176 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "runtime/pprof"
-    "sort"
-    "time"
+	"bufio"
+	"bytes"
+	"golang.org/x/exp/mmap"
+	"fmt"
+	"io"
+	"os"
+	"runtime/pprof"
+	"sort"
+	"time"
 )
 
 const filepath = "../1brc/measurements.txt"
 
-type TempData struct {
-    name  string
-    min   int
-    max   int
-    total int
-    count int
-}
-
 func main() {
-    // Set up profiler
-    f, err := os.Create("cpu_profile.prof")
-    if err != nil {
-        panic("What I can't make a file now?")
-    }
-    pprof.StartCPUProfile(f)
-    defer pprof.StopCPUProfile()
+	f, err := os.Create("cpu_profile.prof")
+	if err != nil { panic("What I can't make a file now?") }
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 
-    // Run
-    startTime := time.Now()
-    attempt()
-	println(time.Since(startTime))
+	startTime := time.Now()
+//	attempt()
+//	offset := 0
+//	var buf []byte
+//	for {
+//		buf, offset = readDataFromFile(offset)
+//		if offset == -1 {
+//			break
+//		}
+//		println("Offset:", offset)
+//		println(string(buf[len(buf)-50:]))
+//		println("---------------------------------------------------------------------------")
+//	}
+	file, _ := os.Open(filepath)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		_ = scanner.Bytes()
+	}
+	fmt.Printf("total time: %s\n", time.Since(startTime))
 }
 
-func attempt() {
-    file, err := os.Open(filepath)
-    if err != nil { panic(err) }
-    defer file.Close()
-
-	m := make(map[string]*TempData, 1000)
-    readTempData(file, &m)
-    process(&m)
+type TempData struct {
+	name  []byte
+	min   int
+	max   int
+	total int
+	count int
 }
 
 func parseTemp(s []byte) int {
-    tenths := 0
-    isNegative := s[0] == '-'
-    i := 0
-    if isNegative { i = 1 }
-    for ; i < len(s); i++ {
-        c := s[i]
-        if c == '.' { continue }
-        rawValue := int(c) - 48
-        tenths *= 10
-        tenths += rawValue
-    }
-    if isNegative { return -tenths }
-    return tenths
+	tenths := 0
+	isNegative := s[0] == '-'
+	i := 0
+	if isNegative {
+		i = 1
+	}
+	for ; i < len(s); i++ {
+		c := s[i]
+		if c == '.' {
+			continue
+		}
+		rawValue := int(c) - 48
+		tenths *= 10
+		tenths += rawValue
+	}
+	if isNegative {
+		return -tenths
+	}
+	return tenths
 }
 
 func split(s []byte) ([]byte, []byte) {
-    for i := 0; i < len(s); i++ {
-        if s[i] == ';' {
-            return s[:i], s[i+1:]
-        }
-    }
-    return s, []byte{}
-}
-
-func readTempData(file *os.File, m *map[string]*TempData) {
-    count := 0
-    scanner := bufio.NewScanner(file)
-    var tempData *TempData
-    var name []byte
-    var temp []byte
-    var f int
-    var ok bool
-    for scanner.Scan() {
-        name, temp = split(scanner.Bytes())
-        f = parseTemp(temp)
-		tempData, ok = (*m)[string(name)]
-        if !ok {
-			(*m)[string(name)] = &TempData{
-                name:  string(name),
-                min:   f,
-                max:   f,
-                total: f,
-                count: 1,
-            }
-        } else {
-            if f < tempData.min { tempData.min = f }
-            if f > tempData.max { tempData.max = f }
-            tempData.total += f
-            tempData.count++
-        }
-        if count%10000000 == 0 {
-            println(count)
-        }
-        count += 1
-    }
-}
-
-func process(m *map[string]*TempData) {
-    startTime := time.Now()
-	var tempDataSlice []TempData
-	for _, v := range *m {
-		tempDataSlice = append(tempDataSlice, *v)
+	for i := 0; i < len(s); i++ {
+		if s[i] == ';' {
+			return s[:i], s[i+1:]
+		}
 	}
-	sort.Slice(tempDataSlice, func(a, b int) bool { return tempDataSlice[a].name < tempDataSlice[b].name })
-    for _, i := range tempDataSlice {
-        avgTemp := float64(i.total/i.count) / 10.0
-        fmt.Printf("%s=%.1f/%.1f/%.1f\n", i.name, float64(i.min)/10.0, avgTemp, float64(i.max)/10.0)
-    }
-    fmt.Printf("process time: %v\n", time.Since(startTime))
+	return s, []byte{}
+}
+
+func readDataFromFile(offset int) ([]byte, int){
+	reader, err := mmap.Open(filepath)
+	if err != nil { panic(err) }
+ 	defer reader.Close()
+
+	bufferSize := 1<<24 //16M
+
+	buf := make([]byte, bufferSize)
+	_, err = reader.ReadAt(buf, int64(offset))
+	if err == io.EOF {
+		// if we hit the end of the file, return the remaining bytes, and signal completion with offset=-1
+		return buf, -1
+	}
+
+	bufferOffset:=0
+	for bufferOffset=bufferSize-1; bufferOffset>=0; bufferOffset-- {
+		b := buf[bufferOffset]
+		if b == '\n' {
+			offset+=bufferOffset
+			break
+		}
+	}
+	return buf[:bufferOffset], offset
+}
+
+func readTempData(file *os.File, hm *HashMap) {
+	count := 0
+	scanner := bufio.NewScanner(file)
+	var tempData *TempData
+	var name []byte
+	var temp []byte
+	var f int
+	var ok bool
+	for scanner.Scan() {
+		name, temp = split(scanner.Bytes())
+		f = parseTemp(temp)
+
+		tempData, ok = hm.Get(name)
+		if !ok {
+			nameCopy := make([]byte, len(name))
+			copy(nameCopy, name)
+
+			hm.Set(nameCopy, &TempData{
+				name:  nameCopy,
+				min:   f,
+				max:   f,
+				total: f,
+				count: 1,
+			})
+		} else {
+			if f < tempData.min {
+				tempData.min = f
+			}
+			if f > tempData.max {
+				tempData.max = f
+			}
+			tempData.total += f
+			tempData.count++
+		}
+		if count%10000000 == 0 {
+			fmt.Printf("read: %d lines, size: %d\n", count, hm.Size())
+		}
+		count += 1
+	}
+}
+
+func process(hm *HashMap) {
+	startTime := time.Now()
+	var data []*TempData
+	for _, v := range hm.buckets {
+		if v != nil {
+			data = append(data, v.Value)
+		}
+	}
+	sort.Slice(data, func(a, b int) bool { return bytes.Compare(data[a].name, data[b].name) < 0 })
+	for _, i := range data {
+		avgTemp := float64(i.total/i.count) / 10.0
+		fmt.Printf("%s=%.1f/%.1f/%.1f\n", i.name, float64(i.min)/10.0, avgTemp, float64(i.max)/10.0)
+	}
+	fmt.Printf("process time: %s\n", time.Since(startTime))
+}
+
+func attempt() {
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	hm := NewHashMap(1000)
+	readTempData(file, hm)
+	process(hm)
 }
 
